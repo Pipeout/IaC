121ce1764c8eb6de7f2e70fbdc6954f48c47150f
aurelio <aurelioserafim2807@gmail.com>
2026-05-24 15:47:08 -0300

    passing variables to env before tofu init

--- providers.tf
+++ providers.tf
@@ -2,11 +2,10 @@ provider "aws" {
   region = var.aws_region
 }
 
-
 terraform {
   backend "s3" {
     bucket = "pipeout-database"
     key    = "infra/terraform.tfstate"
-    region = "us-east-1"
+    region = var.aws_region
   }
 }
